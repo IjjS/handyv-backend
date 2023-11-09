5696190bc3deba95b70faba2d01a9dc0eaad1da3
Jay <injaesong0@gmail.com>
2023-11-10 01:32:55 +0900

    style: rename project module

---
+++ src/main/java/com/programmers/handyV/HandyVApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.handyV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyVApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandyVApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programmers/handyV/HandyVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.handyV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyVApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
