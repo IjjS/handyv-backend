652f80734768af498d6546880c31b3dac3835985
Jay <injaesong0@gmail.com>
2023-11-19 13:06:55 +0900

    fix: validate the full number format splittable by white space

--- src/main/java/com/programmers/handyV/user/domain/CarNumber.java
+++ src/main/java/com/programmers/handyV/user/domain/CarNumber.java
@@ -1,11 +1,14 @@
 package com.programmers.handyV.user.domain;
 
+import com.programmers.handyV.common.exception.BadRequestException;
+
 public class CarNumber {
     private final String frontNumber;
     private final String backNumber;
 
     public CarNumber(String fullNumber) {
         String[] carNumber = fullNumber.split(" ");
+        validateFullNumber(carNumber);
         this.frontNumber = carNumber[0];
         this.backNumber = carNumber[1];
     }
@@ -17,5 +20,11 @@ public class CarNumber {
 
     public String getFullNumber() {
         return frontNumber + " " + backNumber;
+    }
+
+    public void validateFullNumber(String[] carNumber) {
+        if (carNumber.length != 2) {
+            throw new BadRequestException("차 번호 형식이 잘못되었습니다. 띄어쓰기로 구분해주세요 (ex.11가 1111)");
+        }
     }
 }
