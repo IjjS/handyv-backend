bf7ec57ac3cb53d5ed889ef3da16bb716136a321
Jay <injaesong0@gmail.com>
2023-11-22 12:39:31 +0900

    enhance: specify charger types

--- src/main/java/com/programmers/handyV/charger/domain/ChargerType.java
+++ src/main/java/com/programmers/handyV/charger/domain/ChargerType.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public enum ChargerType {
-    STANDARD("완속"),
-    QUICK("급속"),
-    HI("초고속");
+    STANDARD_AC("완속 AC단상"),
+    QUICK_AC3("급속 AC3상"),
+    QUICK_DC_CHADEMO("급속 DC차데모"),
+    QUICK_DC_COMBO("급속 DC콤보"),
+    HI_DC_COMBO("초급속 DC콤보");
 
     private final String chargerTypeName;
 
